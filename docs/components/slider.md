d206d648e3066e5b8030f17f599e0df8afb9992a
Andreas H. Kelch <ak@mausbrand.de>
2021-10-28 07:46:14 +0200

    feat: added Slider configuration and documentation

--- docs/components/slider.md
+++ docs/components/slider.md
@@ -2,7 +2,7 @@
 
 [component-header:sl-slider]
 
-A description of the component goes here.
+This Slider component uses internally the [Glider.js](https://nickpiscitelli.github.io/Glider.js) library. 
 
 ```html preview
 <sl-slider>
@@ -15,12 +15,29 @@ A description of the component goes here.
 
 ## Examples
 
-### First Example
+### Show more Slides 
 
-TODO
+Use the `slidesToShow` attribute to set the amount of visible slides.
+```html preview
+<sl-slider slidesToShow=2>
+  <div><img src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&sat=-100&bri=-5"/></div>
+  <div><img src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"/></div>
+  <div><img src="https://images.unsplash.com/photo-1520903074185-8eca362b3dce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"/></div>
+  <div><img src="https://images.unsplash.com/photo-1520640023173-50a135e35804?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"/></div>
+</sl-slider>
+```
 
-### Second Example
+### Autoscroll to Slides 
 
-TODO
+Use the `autoScroll` attribute to set the scroll interval.
+
+```html preview
+<sl-slider autoScroll=5000>
+  <div><img src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&sat=-100&bri=-5"/></div>
+  <div><img src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"/></div>
+  <div><img src="https://images.unsplash.com/photo-1520903074185-8eca362b3dce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"/></div>
+  <div><img src="https://images.unsplash.com/photo-1520640023173-50a135e35804?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"/></div>
+</sl-slider>
+```
 
 [component-metadata:sl-slider]
--- src/components/slider/slider.ts
+++ src/components/slider/slider.ts
@@ -8,38 +8,111 @@ import Glider from './glider.js'
  * @status experimental
  * @viur 0.5
  *
- * @dependency sl-example
- *
- * @event sl-event-name - Emitted as an example.
- *
- * @slot - The default slot.
+ * @slot - Insert HTML Elements which will be used as slides
  *
  * @csspart base - The component's base wrapper.
+ * @csspart dots - The component's dots wrapper.
+ * @csspart previous-button - The component's previous Button.
+ * @csspart next-button - The component's next Button.
  *
- * @cssproperty --example - An example CSS custom property.
+ * @cssproperty --slider-dot-color - The dot color.
+ * @cssproperty --slider-arrow-color - The arrow color.
+ * @cssproperty --slider-arrow-color-hover - The arrow hover color.
  */
 @customElement('sl-slider')
 export default class SlSlider extends LitElement {
   static styles = styles;
   @query('.glider') gliderWrapper: HTMLElement;
 
-  baseOptions = {
-    slidesToShow: 1,
-      draggable: true,
-      dots: '.dots',
-      arrows: {
+  /** Wenn set arrows are rendered */
+  @property({ type: Boolean, reflect: true }) arrows = true;
+
+  /** Wenn set navigation dots are rendered */
+  @property({ type: Boolean, reflect: true }) dots = true;
+
+  /** The amount of visible Slides */
+  @property({ type: Number, reflect: true }) slidesToShow = 1;
+
+  /** This value is ignored unless slidesToShow is set to auto, in which it is then required. */
+  @property({ type: Number, reflect: true }) itemWidth = undefined;
+
+  /** An aggravator used to control animation speed. Higher is slower! */
+  @property({ type: Number, reflect: true }) duration = 0.5;
+
+  /** Wenn set the slides are draggable */
+  @property({ type: Boolean, reflect: true }) draggable = true;
+
+  /** If true, Glider.js will scroll to the nearest slide after any scroll interactions */
+  @property({ type: Boolean, reflect: true }) scrollLock = true;
+
+  /** If true, Glider.js will lock to the nearest slide on resizing of the window */
+  @property({ type: Boolean, reflect: true }) resizeLock = true;
+
+  /** If true, Glider.js will scroll to the beginning/end when its respective endpoint is reached */
+  @property({ type: Boolean, reflect: true }) rewind = true;
+
+  /** This value is ignored unless rewind is set to true. Amount of second till next slide */
+  @property({ type: Number, reflect: true }) autoScroll = 0;
+
+  baseOptions = {}
+  gliderInstance = null
+  interval = null
+
+  collectOtions(){
+    if (this.arrows){
+      Object.assign(this.baseOptions,{'arrows':{
         prev: '.prev',
         next: '.next',
-            skipTrack: true
-      }
+      }})
     }
 
-  firstUpdated(){
-    //new Glider( this.gliderWrapper, this.baseOptions)
+    if (this.dots){
+      Object.assign(this.baseOptions, {"dots":'.dots'})
+    }
+
+    Object.assign(this.baseOptions,{
+      "slidesToShow": this.slidesToShow === 0?"auto":this.slidesToShow,
+      "slidesToScroll":this.slidesToShow === 0?1:this.slidesToShow,
+      "draggable": this.draggable,
+      "duration": this.duration,
+      "scrollLock": this.scrollLock,
+      "resizeLock": this.resizeLock,
+      "rewind": this.rewind,
+      "itemWidth":this.itemWidth
+    })
+  }
+
+  startAutoScroll(){
+    if (this.interval){
+      clearInterval(this.interval)
+    }
+    if (this.autoScroll !== 0 && this.rewind){
+       this.interval = setInterval(()=>{
+       if(this.gliderInstance){
+         this.gliderInstance.scrollItem("next")
+       }
+    },this.autoScroll)
+    }
+
+  }
+
+
+  hasChanged(){
+    if(this.gliderInstance){
+      this.gliderInstance.destroy()
+    }
+
+    this.collectOtions()
+    console.log(this.baseOptions)
+    this.gliderInstance = new Glider( this.gliderWrapper, this.baseOptions)
+    this.startAutoScroll()
   }
 
   handleSlotChange(){
-    new Glider( this.gliderWrapper, this.baseOptions)
+    this.collectOtions()
+    console.log(this.baseOptions)
+    this.gliderInstance = new Glider( this.gliderWrapper, this.baseOptions)
+    this.startAutoScroll()
   }
 
   render() {
