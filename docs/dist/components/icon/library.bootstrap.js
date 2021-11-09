c1faa9953983f503300d081165e7c7889019c01e
Andreas H. Kelch <ak@mausbrand.de>
2021-11-09 09:56:48 +0100

    remove doc build

--- docs/dist/components/icon/library.bootstrap.js
+++
@@ -1,8 +0,0 @@
-import {
-  library_bootstrap_default
-} from "../../chunks/chunk.I7E4YVM6.js";
-import "../../chunks/chunk.S6TJZ6NJ.js";
-import "../../chunks/chunk.IHGPZX35.js";
-export {
-  library_bootstrap_default as default
-};
