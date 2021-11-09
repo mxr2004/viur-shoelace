434926175d2588fec7faa1bb73dc8e9d562f0585
Andreas H. Kelch <ak@mausbrand.de>
2021-11-09 10:47:25 +0100

    removed prebuild docu

--- docs/dist/components/format-number/format-number.js
+++
@@ -1,9 +0,0 @@
-import {
-  format_number_default
-} from "../../chunks/chunk.LSN4LC7Z.js";
-import "../../chunks/chunk.JPCNAH5U.js";
-import "../../chunks/chunk.X3WLUTHF.js";
-import "../../chunks/chunk.IHGPZX35.js";
-export {
-  format_number_default as default
-};
--- docs/dist/components/icon/library.default.js
+++
@@ -1,8 +0,0 @@
-import {
-  library_default_default
-} from "../../chunks/chunk.XQSM4HF3.js";
-import "../../chunks/chunk.S6TJZ6NJ.js";
-import "../../chunks/chunk.IHGPZX35.js";
-export {
-  library_default_default as default
-};
